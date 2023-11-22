fc9b65b05a43cd8b03c94c727b57ace57731234c
Pinukio <gkswjddnjs15@gmail.com>
2023-11-23 01:12:33 +0900

    add script

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotateSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            this.rotateSpeed = 10;
+        }
+        transform.Rotate(0, 0, this.rotateSpeed); //Z축을 중심으로 함
+        this.rotateSpeed *= 0.995f;
+    }
+}
